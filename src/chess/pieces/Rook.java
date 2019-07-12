0a620f88198ef799fc49f8c959bb7066db4cb5bf
WilkerLira <34160767+WilkerLira@users.noreply.github.com>
2019-07-11 22:17:10 -0300

    Sub-Classe King, Rook

---
+++ src/chess/pieces/Rook.java
@@ -0,0 +1,19 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Rook extends ChessPiece{
+
+	public Rook(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		//R, será a peça que aparece no tabuleiro
+		return "R";
+	}
+
+}
